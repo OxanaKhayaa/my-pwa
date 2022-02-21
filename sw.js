f2268b1e6d6a07f9e9c27ebaf4998fc92c01b8d5
Oxana <83176987+oxanalearn@users.noreply.github.com>
2022-02-21 18:31:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,6 +9,6 @@ self.addEventListener('activate', function(event) {
 });
 
 //**self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ....', event);
-  event.respondWith(fetch(event.request));
-});**/
+ //** console.log('[Service Worker] Fetching something ....', event);
+//**  event.respondWith(fetch(event.request));
+//**});
