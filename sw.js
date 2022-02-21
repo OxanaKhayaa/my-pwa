69fd0b7fd5094bdb1c7ed38f3cb0003e659abaae
Oxana <83176987+oxanalearn@users.noreply.github.com>
2022-02-21 14:12:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('sw.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ...', event);
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetching something ....', event);
+  event.respondWith(fetch(event.request));
+});
