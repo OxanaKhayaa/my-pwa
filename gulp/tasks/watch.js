b5fc977730f6c6e2e9780a4e238458ffba7bfcd9
name <oxana81.test@gmail.com>
2022-04-04 20:02:51 +0300

    pwa

--- gulp/tasks/watch.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const {watch, parallel, series} = require("gulp");
-
-module.exports = function watching() {
-    watch('src/**/*.scss', parallel('style'));
-    watch('src/**/*.html', parallel('html'));
-}
