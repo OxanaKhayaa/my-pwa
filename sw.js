94b5002e4af501011a8996d957ccd1e9282cf151
name <oxana81.test@gmail.com>
2022-04-13 13:10:19 +0300

    pwa

--- sw.js
+++ sw.js
@@ -1,9 +1,36 @@
-var cacheName = 'hello-pwa';
+var cacheName = 'cache-site-element';
 var filesToCache = [
     'index.html',
+    'login.html',
     'my-awards.html',
+    'new-patient.html',
+    'passwordreset.html',
+    'refer-and-earn.html',
+    'ways-to-earn.html',
+    'shop.html',
     'css/style.min.css',
-    'js/main.js'
+    'js/main.js',
+    'js/mobile-swipe-menu.min.js',
+    'js/modernizr-custom.js',
+    'image/logo.svg',
+    'image/logo-home.svg',
+    'image/girl-with-phone.jpg',
+    'image/girl-with-phone.webp',
+    'image/man-with-phone.jpg',
+    'image/man-with-phone.webp',
+    'image/man-with-phone.jpg',
+    'image/menu-bg.webp',
+    'image/menu-bg.jpg',
+    'image/shop-page/brand-name.webp',
+    'image/shop-page/brand-name.jpg',
+    'image/shop-page/concert.webp',
+    'image/shop-page/concert.jpg',
+    'image/shop-page/digital-gift.webp',
+    'image/shop-page/digital-gift.jpg',
+    'image/shop-page/local-shopping.webp',
+    'image/shop-page/local-shopping.jpg',
+    'image/shop-page/resort.webp',
+    'image/shop-page/resort.jpg'
 ];
 
 /* Start the service worker and cache all of the app's content */
