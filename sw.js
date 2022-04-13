adc12b05dbfd9baa8b0f5b2b004e3553d4f4ab3f
name <oxana81.test@gmail.com>
2022-04-13 17:40:18 +0300

    pwa

--- sw.js
+++ sw.js
@@ -1,5 +1,39 @@
 const CACHE_NAME = 'offline';
-const OFFLINE_URL = ['offline.html', 'index.html', 'css/style.min.css', 'login.html'];
+const OFFLINE_URL = 'offline.html';
+
+let cacheName = 'cache-site-element';
+let filesToCache = [
+    'index.html',
+    'login.html',
+    'my-awards.html',
+    'new-patient.html',
+    'passwordreset.html',
+    'refer-and-earn.html',
+    'ways-to-earn.html',
+    'shop.html',
+    'css/style.min.css',
+    'js/main.js',
+    'js/mobile-swipe-menu.min.js',
+    'js/modernizr-custom.js',
+    'image/logo.svg',
+    'image/logo-home.svg',
+    'image/girl-with-phone.jpg',
+    'image/girl-with-phone.webp',
+    'image/man-with-phone.jpg',
+    'image/man-with-phone.webp',
+    'image/menu-bg.webp',
+    'image/menu-bg.jpg',
+    'image/shop-page/brand-name.webp',
+    'image/shop-page/brand-name.jpg',
+    'image/shop-page/concert.webp',
+    'image/shop-page/concert.jpg',
+    'image/shop-page/digital-gift.webp',
+    'image/shop-page/digital-gift.jpg',
+    'image/shop-page/local-shopping.webp',
+    'image/shop-page/local-shopping.jpg',
+    'image/shop-page/resort.webp',
+    'image/shop-page/resort.jpg'
+];
 
 self.addEventListener('install', function(event) {
     console.log('[ServiceWorker] Install');
@@ -13,6 +47,18 @@ self.addEventListener('install', function(event) {
 
     self.skipWaiting();
 });
+
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+
+
 
 self.addEventListener('activate', (event) => {
     console.log('[ServiceWorker] Activate');
@@ -49,4 +95,13 @@ self.addEventListener('fetch', function(event) {
             }
         })());
     }
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
 });
