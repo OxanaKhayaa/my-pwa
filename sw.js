8af739771df828cb4ecd67054baf4207044b3c6e
name <oxana81.test@gmail.com>
2022-04-13 17:29:24 +0300

    pwa

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', function(event) {
         const cache = await caches.open(CACHE_NAME);
         // Setting {cache: 'reload'} in the new request will ensure that the response
         // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-        await cache.addAll(new Request(OFFLINE_URL, {cache: 'reload'}));
+        await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
     })());
 
     self.skipWaiting();
