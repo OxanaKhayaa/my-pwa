8af3b7bb9a44c1fc77df917c713ebef2653f1775
name <oxana81.test@gmail.com>
2022-04-13 11:16:46 +0300

    pwa

--- sw.js
+++ sw.js
@@ -1,113 +1,69 @@
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'offline.html';
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 
 self.addEventListener('install', function(event) {
-    console.log('[ServiceWorker] Install');
-
-    event.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        // Setting {cache: 'reload'} in the new request will ensure that the response
-        // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-        await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-    })());
-
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    console.log('[ServiceWorker] Activate');
-    event.waitUntil((async () => {
-        // Enable navigation preload if it's supported.
-        // See https://developers.google.com/web/updates/2017/02/navigation-preload
-        if ('navigationPreload' in self.registration) {
-            await self.registration.navigationPreload.enable();
-        }
-    })());
-
-    // Tell the active service worker to take control of the page immediately.
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', function(event) {
-    // console.log('[Service Worker] Fetch', event.request.url);
-    if (event.request.mode === 'navigate') {
-        event.respondWith((async () => {
-            try {
-                const preloadResponse = await event.preloadResponse;
-                if (preloadResponse) {
-                    return preloadResponse;
-                }
-
-                const networkResponse = await fetch(event.request);
-                return networkResponse;
-            } catch (error) {
-                console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
-
-                const cache = await caches.open(CACHE_NAME);
-                const cachedResponse = await cache.match(OFFLINE_URL);
-                return cachedResponse;
-            }
-        })());
-    }
-});
-
-
-// Immediately take control of the page, see the 'Immediate Claim' recipe
-// for a detailed explanation of the implementation of the following two
-// event listeners.
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(self.skipWaiting());
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME)
+            .then(function(cache) {
+                console.log('[Service Worker] Precaching App Shell');
+                cache.addAll([
+                    'index.html',
+                    'login.html',
+                    'refer-and-earn.html',
+                    'ways-to-earn.html',
+                    'style.min.css'
+                    /*'/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/promise.js',
+                    '/src/js/fetch.js',
+                    '/src/js/material.min.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/main-image.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'*/
+                ]);
+            })
+    )
 });
 
 self.addEventListener('activate', function(event) {
-    event.waitUntil(self.clients.claim());
-});
-
-// Register event listener for the 'push' event.
-self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Activating Service Worker ....', event);
     event.waitUntil(
-        // Retrieve a list of the clients of this service worker.
-        self.clients.matchAll().then(function(clientList) {
-            // Check if there's at least one focused client.
-            var focused = clientList.some(function(client) {
-                return client.focused;
-            });
-
-            var notificationMessage;
-            if (focused) {
-                notificationMessage = 'You\'re still here, thanks!';
-            } else if (clientList.length > 0) {
-                notificationMessage = 'You haven\'t closed the page, ' +
-                    'click here to focus it!';
-            } else {
-                notificationMessage = 'You have closed the page, ' +
-                    'click here to re-open it!';
-            }
-
-            // Show a notification with title 'ServiceWorker Cookbook' and body depending
-            // on the state of the clients of the service worker (three different bodies:
-            // 1, the page is focused; 2, the page is still open but unfocused; 3, the page
-            // is closed).
-            return self.registration.showNotification('ServiceWorker Cookbook', {
-                body: notificationMessage,
-            });
-        })
+        caches.keys()
+            .then(function(keyList) {
+                return Promise.all(keyList.map(function(key) {
+                    if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                        console.log('[Service Worker] Removing old cache.', key);
+                        return caches.delete(key);
+                    }
+                }));
+            })
     );
+    return self.clients.claim();
 });
 
-// Register event listener for the 'notificationclick' event.
-self.addEventListener('notificationclick', function(event) {
-    event.waitUntil(
-        // Retrieve a list of the clients of this service worker.
-        self.clients.matchAll().then(function(clientList) {
-            // If there is at least one client, focus it.
-            if (clientList.length > 0) {
-                return clientList[0].focus();
-            }
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                } else {
+                    return fetch(event.request)
+                        .then(function(res) {
+                            return caches.open(CACHE_DYNAMIC_NAME)
+                                .then(function(cache) {
+                                    cache.put(event.request.url, res.clone());
+                                    return res;
+                                })
+                        })
+                        .catch(function(err) {
 
-            // Otherwise, open a new page.
-            return self.clients.openWindow('offline.html');
-        })
+                        });
+                }
+            })
     );
 });
