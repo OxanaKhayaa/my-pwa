3e1010506bbd11483f4337f606ec64f59661084e
name <oxana81.test@gmail.com>
2022-04-13 14:06:46 +0300

    pwa

--- sw.js
+++ sw.js
@@ -1,51 +1,89 @@
-var cacheName = 'cache-site-element';
-var filesToCache = [
-    'index.html',
-    'login.html',
-    'my-awards.html',
-    'new-patient.html',
-    'passwordreset.html',
-    'refer-and-earn.html',
-    'ways-to-earn.html',
-    'shop.html',
-    'css/style.min.css',
-    'js/main.js',
-    'js/mobile-swipe-menu.min.js',
-    'js/modernizr-custom.js',
-    'image/logo.svg',
-    'image/logo-home.svg',
-    'image/girl-with-phone.jpg',
-    'image/girl-with-phone.webp',
-    'image/man-with-phone.jpg',
-    'image/man-with-phone.webp',
-    'image/menu-bg.webp',
-    'image/menu-bg.jpg',
-    'image/shop-page/brand-name.webp',
-    'image/shop-page/brand-name.jpg',
-    'image/shop-page/concert.webp',
-    'image/shop-page/concert.jpg',
-    'image/shop-page/digital-gift.webp',
-    'image/shop-page/digital-gift.jpg',
-    'image/shop-page/local-shopping.webp',
-    'image/shop-page/local-shopping.jpg',
-    'image/shop-page/resort.webp',
-    'image/shop-page/resort.jpg'
-];
+/*
+© Google LLC, 2015, 2019, 2020, 2021. Все права защищены.
+ Данный файл лицензирован на условиях лицензии Apache License 2.0 ("Лицензия");
+ использовать этот файл разрешено только в соответствии с Лицензией.
+ Текст Лицензии можно получить по адресу
+ http://www.apache.org/licenses/LICENSE-2.0
+ За исключением случаев, предусмотренных действующим законодательством или согласованных в письменной форме, программное обеспечение,
+ распространяемое на условиях Лицензии, предоставляется на УСЛОВИЯХ "КАК ЕСТЬ",
+ БЕЗ КАКИХ-ЛИБО ГАРАНТИЙ ИЛИ УСЛОВИЙ, явных или подразумеваемых.
+ Сведения об основных разрешениях и ограничениях в рамках Лицензии для определенных языков
+ см. в тексте Лицензии.
+*/
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
+// Если увеличить значение переменной OFFLINE_VERSION, будет создано событие установки и
+// ранее кэшированные ресурсы будут обновлены из сети.
+// Эта переменная намеренно объявлена и не используется.
+// Если нужно, добавьте комментарий для своего анализатора кода:
+// eslint-disable-next-line no-unused-vars
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = "offline";
+// При необходимости укажите здесь другой URL-адрес.
+const OFFLINE_URL = "offline.html";
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            // Если настроить {cache: 'reload'} в новом запросе,
+            // ответ гарантированно не будет получен из кэша HTTP; т. е. он будет получен из
+            // сети.
+            await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+        })()
     );
+    // Принудительный перевод ожидающего служебного сценария в активное состояние.
+    self.skipWaiting();
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        (async () => {
+            // Включение предварительной загрузки при переходе между страницами, если эта функция поддерживается.
+            // См. сведения по ссылке https://developers.google.com/web/updates/2017/02/navigation-preload
+            if ("navigationPreload" in self.registration) {
+                await self.registration.navigationPreload.enable();
+            }
+        })()
     );
+
+    // Сообщаем активному служебному сценарию, что необходимо немедленно получить контроль над страницей.
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    // Нам нужно вызвать функцию event.respondWith(), только если это запрос на переход между
+    // HTML-страницами.
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            (async () => {
+                try {
+                    // Прежде всего попытаемся использовать ответ предварительной загрузки при переходе между страницами, если эта функция поддерживается.
+                    const preloadResponse = await event.preloadResponse;
+                    if (preloadResponse) {
+                        return preloadResponse;
+                    }
+
+                    // Всегда сначала проверяйте сеть.
+                    const networkResponse = await fetch(event.request);
+                    return networkResponse;
+                } catch (error) {
+                    // Событие catch появляется только при возникновении исключения, которое, вероятно,
+                    // вызвано ошибкой сети.
+                    // Если функция fetch() возвращает допустимый ответ HTTP с кодом ответа в
+                    // диапазоне 4xx или 5xx, функция catch() НЕ будет вызвана.
+                    console.log("Не удалось получить данные; вместо этого возвращаем страницу для автономного режима.", error);
+
+                    const cache = await caches.open(CACHE_NAME);
+                    const cachedResponse = await cache.match(OFFLINE_URL);
+                    return cachedResponse;
+                }
+            })()
+        );
+    }
+
+    // Если выражение в условии if() ложно, то этот обработчик операции получения данных не перехватит
+    // запрос. Если зарегистрированы любые другие обработчики операций получения данных, они
+    // смогут вызвать метод event.respondWith(). Если ни один обработчик операций получения данных не вызовет метод
+    // event.respondWith(), браузер обработает запрос таким образом, как если бы
+    // не были задействованы никакие служебные сценарии.
 });
